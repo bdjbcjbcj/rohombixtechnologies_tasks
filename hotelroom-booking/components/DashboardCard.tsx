9134d84e0ab35b5706421253e164302524123a0c
ZunairHafeez <zunairhafeez404@gmail.com>
2026-07-26 02:08:31 +0500

    blog

--- hotelroom-booking/components/DashboardCard.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardCard({ title, value }) {
-  return (
-    <div className="bg-white p-6 rounded-xl shadow-md">
-      <h3 className="text-gray-500 text-lg">{title}</h3>
-      <p className="text-3xl font-bold mt-2">{value}</p>
-    </div>
-  )
-}
